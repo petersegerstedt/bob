b92517cd86f304b745993f8f52ae6cdad77026eb
Peter Segerstedt <petersegerstedt@users.noreply.github.com>
2020-11-08 22:26:25 +0100

    trying to fix strange (to me) ownership detail

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-
+// https://serde.rs/borrow.html
+// https://github.com/serde-rs/serde-rs.github.io/issues/57
 pub mod gitea {
     use serde::{Serialize, Deserialize};
+    use std::borrow::Cow;
     //use serde::de::{IgnoredAny};
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Msg<'a> {
         //pub secret: Option<&'a str>,
-        pub before: &'a str, //"28e1879d02 9cb852e484 4d9c718537 df08844e03",
-        pub after: &'a str, //"bffeb74224043ba2feb48d137756c8a9331c449a",
+        pub before: Cow<'a, str>, //"28e1879d02 9cb852e484 4d9c718537 df08844e03",
+        pub after: Cow<'a, str>, //"bffeb74224043ba2feb48d137756c8a9331c449a",
         //compare_url: Option<&'a str>,
         pub commits: Option<Vec<Commit<'a>>> ,
         //repository: Option<IgnoredAny> ,
@@ -31,26 +33,26 @@ pub mod gitea {
         #[serde(skip_serializing_if = "Option::is_none")]
         pub id: Option<u32>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        pub login: Option<&'a str>,
+        pub login: Option<Cow<'a, str>>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        pub name: Option<&'a str>,
+        pub name: Option<Cow<'a, str>>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        pub full_name: Option<&'a str>,
-        pub email: &'a str,
+        pub full_name: Option<Cow<'a, str>>,
+        pub email: Cow<'a, str>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        pub avatar_url: Option<&'a str>,
-        pub username: &'a str,
+        pub avatar_url: Option<Cow<'a, str>>,
+        pub username: Cow<'a, str>,
     }
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Commit<'a> {
-        pub id: &'a str,
-        pub message: &'a str,
+        pub id: Cow<'a, str>,
+        pub message: Cow<'a, str>,
         //pub url: &'a str,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub author: Option<User<'a>>,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub committer: Option<User<'a>>,
-        pub timestamp: &'a str, 
+        pub timestamp: Cow<'a, str>, 
     }
 }
 
